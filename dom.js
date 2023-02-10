003800ddc43b1247a8f4e379a2b26a3d66a895f1
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-02-10 22:11:45 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,20 @@
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all[10]);
+// document.activeElement.all[10].textcontent = "hello";
+var headerTitle = document.getElementById("header-title");
+var header = document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Goodbye";
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML = "<h3>Hello</h3>";
+header.style.border = "solid 3px #000";
+document.getElementById("main").style.color = "green";
+document.getElementByclass("main").style.fontWeight = "900";
+
+
